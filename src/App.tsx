f36bd876948364caa5abba53381889ae60ab3883
Júnior Nunes <denisonprobable13@gmail.com>
2023-09-11 22:10:17 -0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
         <h1 className="text-xl font-bold">upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com ❤️ </span>
+          <span className="text-sm text-muted-foreground">Developed with ❤️ </span>
           <Separator orientation='vertical'   className='h-6' />
           <Button variant="outline" size="lg">
             <Github  className='w-4 h-4 mr-2' />
@@ -20,7 +20,13 @@ export function App() {
       </div>
 
       <main className='flex-1 p-6 flex gap-6'>
-        <div className='flex flex-col flex-1'></div>
+        <div className='flex flex-col flex-1 gap-4'>
+          <div className='grid grid-rows-2  gap-4 flex-1 '>
+
+          </div>
+
+          <p className='text-sm text-muted-foreground'>Remember, do you need to use the "transcription" variable in order to add the selected video content</p>
+        </div>
         <aside className='w-80'></aside>
       </main>
     </div>
